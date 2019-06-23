9372d6809fccede9bafc6613817edcacd789da0f
larisarysbekova <user@email.com>
2019-06-23 22:07:23 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random r = new Random();
+        int randoma = r.nextInt(107) - 0;
+        System.out.println("Здрвствуйте, Вам " + randoma + " лет");
+
+        System.out.println("Как вас зовут?");
+        Scanner word = new Scanner(System.in);
+        String name = word.nextLine();
+        int age = randoma;
+        System.out.println("Какая температура на улице?");
+        int temperature = word.nextInt();
+        if (temperature > 30 || temperature < -20 && age <= 20 || age > 45 ) {
+            System.out.println(name + " Вы не можете выйти на прогулку");
+        }
+        else if (temperature <= 0 || temperature > 28 && age < 20 ) {
+            System.out.println(name + " Вы не можете выйти на прогулку");
+        }
+        else if (temperature < -10 || temperature > 25 && age >= 45) {
+            System.out.println(name + " Вы не можете выйти на прогулку");
+        } else {
+            System.out.println(name + " Вы можете пойти в гости");
+        }
+
+    }
+}
